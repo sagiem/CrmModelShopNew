50b987233b54eaa023f71607366433799bb5eac9
sagiem <sagiems@gmail.com>
2021-03-03 16:26:50 +0600

    Добавьте файлы проекта.

---
+++ CrmBl/Model/Sell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    public class Sell
+    {
+        public int SellId { get; set; }
+        public int ChekId { get; set; }
+        public int ProductId { get; set; }
+
+        public virtual Check Check { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
+
